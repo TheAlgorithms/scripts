1276e701e294abaa6b76977966b72a8f35eb2e1e
Christian Clauss <cclauss@me.com>
2021-09-02 09:12:34 +0200

    Reformat build_directory_md.py with psf/black

--- build_directory_md.py
+++ build_directory_md.py
@@ -4,34 +4,40 @@ import os
 import sys
 from typing import Iterator
 
-if(".py" in sys.argv[0]):
+if ".py" in sys.argv[0]:
     sys.argv.pop(0)
 
 
-if(len(sys.argv) < 3 or len(sys.argv) > 5):
+if len(sys.argv) in (3, 4, 5):
     print(
-        "Arguments:\n[0] - Language\n[1] - Base path\n[2] - Allowed filenames\n[3] - Files or folders to ignore (optional)\n[4] - Folders to ignore, but include children (optional)")
-    exit(1)
+        "Arguments:\n"
+        "[0] - Language\n"
+        "[1] - Base path\n"
+        "[2] - Allowed filenames\n"
+        "[3] - Files or folders to ignore (optional)\n"
+        "[4] - Folders to ignore, but include children (optional)"
+    )
+    sys.exit()
 
 ignore = []
 skip = []
-if(len(sys.argv) == 4):
+if len(sys.argv) == 4:
     ignore = sys.argv[3].split(",")
-if(len(sys.argv) == 5):
+if len(sys.argv) == 5:
     skip = sys.argv[4].split(",")
 
-URL_BASE = "https://github.com/TheAlgorithms/" + \
-    sys.argv[0] + "/blob/master"
+URL_BASE = "https://github.com/TheAlgorithms/" + sys.argv[0] + "/blob/master"
 
 
 def good_file_paths(top_dir: str = ".") -> Iterator[str]:
     for dir_path, dir_names, filenames in os.walk(top_dir):
-        dir_names[:] = [d for d in dir_names if d !=
-                        "scripts" and d[0] not in "._"]
+        dir_names[:] = [d for d in dir_names if d != "scripts" and d[0] not in "._"]
         for filename in filenames:
             if filename == "__init__.py":
                 continue
-            if any(e.lower() in os.path.join(dir_path, filename).lower() for e in ignore):
+            if any(
+                e.lower() in os.path.join(dir_path, filename).lower() for e in ignore
+            ):
                 continue
             if os.path.splitext(filename)[1] in sys.argv[2].split(","):
                 path = os.path.join(dir_path, filename).lstrip(".").lstrip("/")
@@ -68,5 +74,3 @@ def print_directory_md(top_dir: str = ".") -> None:
 
 if __name__ == "__main__":
     print_directory_md(sys.argv[1])
-
-
